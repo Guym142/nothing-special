f4085f0b4a00430ec43004aa8788aa9496284f22
TKratter <tomkratter@gmail.com>
2022-02-24 22:16:08 +0200

    person and project basics

--- project.py
+++ project.py
@@ -11,11 +11,23 @@ class Project:
         self._skills_list = project_dict['project_number_of_roles']
         self.skills = [(skill_name, skill_level, self) for skill_name, skill_level in self._skills_list]
         self.persons_in_project = []
-        self.done = False
 
     def add_person(self, person: Person):
         persons_in_project_num = len(self.persons_in_project)
-        if self.len(self.persons_in_project) >= len(self.skills):
-            raise IndexError('too many people in project')
-        if person.skills[self.skills[persons_in_project_num - 1]][0] <
+        if len(self.persons_in_project) >= len(self.skills):
+            raise IndexError('too many persons in project')
+        if person.skills[self.skills[persons_in_project_num - 1][0]] + 1 < self.skills[persons_in_project_num - 1][1]:
+            raise IndexError('person is unfit for assignment')
         self.persons_in_project.append(person)
+
+    def is_full(self) -> bool:
+        return len(self.persons_in_project) == len(self.skills)
+
+    def add_one_to_peoples_skills(self):
+        for i, person in enumerate(self.persons_in_project):
+            if (person.skills[self.skills[i][0]] == self.skills[i][1]) or (
+                    person.skills[self.skills[i][0]] + 1 == self.skills[i][1]):
+                person.skills[self.skills[i][0]] += 1
+
+    def clean(self):
+        self.persons_in_project = []
