ee348ec86c356caf30b4200a7c7f6211c207f019
Guy Morag <xpronet4@gmail.com>
2022-02-25 12:21:05 +0200

    last fixes

--- algorithm.py
+++ algorithm.py
@@ -19,7 +19,7 @@ class Algorithm:
             person_list.append(Person(people_name, skills_dict))
         self.people = People(person_list, skills_set)
         self.write_every = write_every
-        self.available_people = np.ones(len(projects_dict), dtype=bool)
+        self.available_people = np.ones(len(persons_dict), dtype=bool)
         self.running_projects = set()
 
     def update_available_people(self):
@@ -50,14 +50,14 @@ class Algorithm:
 
         # effective_skills = np.divide(relevant_people[skill[0]], relevant_people.sum(axis=1))
         # selected_person = effective_skills.argmin()
-        bla = relevant_people.iloc[0].index
+        bla = relevant_people.index[0]
         return self.people.name_to_person(bla)
 
     def set_working_people(self, project):
         for person in project.persons_in_project:
             self.available_people[person.idx] = 0
 
-    def calculate(self, top_k=1, iters=1):
+    def calculate(self, top_k=3, iters=10):
         self.schedule = []
         self.time = 0
 
@@ -76,7 +76,7 @@ class Algorithm:
                     person = self.find_fitting_person(skill)
                     if person is None:
                         continue
-                    skill[2].add_person(person)
+                    skill[2].add_person(person, skill[0])
 
                 for project in top_k_projects:
                     if project.is_full():
@@ -89,8 +89,9 @@ class Algorithm:
                     else:
                         project.clean()
 
-            if (self.time + 1) % self.write_every == 0:
-                write_file(self.schedule, self.example)
+            # if (self.time + 1) % self.write_every == 0:
+            print('write')
+            write_file(self.schedule, self.example)
 
             self.time += 1
 
@@ -102,7 +103,7 @@ def main():
     parser.add_argument("-e", "--example", help="Example Letter", type=str)
     args = parser.parse_args()
 
-    example = 'a' # args.example
+    example = 'd' # args.example
 
     people_dict, projects_dict, skills_set = load_example(example)
 
--- project.py
+++ project.py
@@ -10,24 +10,33 @@ class Project:
         self.number_of_roles = project_dict['project_number_of_roles']
         self._skills_list = project_dict['project_skills_list']
         self.skills = [(skill_name, skill_level, self) for skill_name, skill_level in self._skills_list]
-        self.persons_in_project = []
+        self.persons_in_project = [None] * len(self._skills_list)
+        self.skills_name = [s[0] for s in self._skills_list]
 
-    def add_person(self, person: Person):
-        persons_in_project_num = len(self.persons_in_project)
-        if len(self.persons_in_project) >= len(self.skills):
-            raise IndexError('too many persons in project')
-        if person.skills[self.skills[persons_in_project_num - 1][0]] + 1 < self.skills[persons_in_project_num - 1][1]:
-            raise IndexError('person is unfit for assignment')
-        self.persons_in_project.append(person)
+    def add_person(self, person: Person, skill: str):
+        skill_id = self.skills_name.index(skill)
+        if skill_id is None:
+            raise ValueError('skill not found')
+        # persons_in_project_num = len(self.persons_in_project)
+        # if len(self.persons_in_project) >= len(self.skills):
+        #     raise IndexError('too many persons in project')
+        # if person.skills[self.skills[persons_in_project_num - 1][0]] + 1 < self.skills[persons_in_project_num - 1][1]:
+        #     raise IndexError('person is unfit for assignment')
+        self.persons_in_project[skill_id] = person
 
     def is_full(self) -> bool:
-        return len(self.persons_in_project) == len(self.skills)
+        for person in self.persons_in_project:
+            if person is None:
+                return False
+        return True
 
     def add_one_to_peoples_skills(self):
         for i, person in enumerate(self.persons_in_project):
+            if person is None:
+                raise ValueError('project not full')
             if (person.skills[self.skills[i][0]] == self.skills[i][1]) or (
                     person.skills[self.skills[i][0]] + 1 == self.skills[i][1]):
                 person.skills[self.skills[i][0]] += 1
 
     def clean(self):
-        self.persons_in_project = []
+        self.persons_in_project = [None] * len(self._skills_list)
